bf9d3ab91575646a06af79f6cb081dfdf7c53fb7
lamNehaM <88654457+lamNehaM@users.noreply.github.com>
2022-09-24 12:17:57 +0530

    Create AddNaturalNum.java

---
+++ AddNaturalNum.java
@@ -0,0 +1,16 @@
+public class AddNaturalNum {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num = 50, sum = 0;
+
+        for(int i = 1; i <= num; ++i)
+        {
+            // sum = sum + i;
+            sum += i;
+        }
+
+        System.out.println("Sum = " + sum);
+	}
+
+}
